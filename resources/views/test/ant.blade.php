b65c5e7ec70213eaa98c6109c12e78f46fd23945
Amornthep65122420004 <150641562+Amornthep65122420004@users.noreply.github.com>
2023-11-20 12:01:09 +0700

    Initial commit

---
+++ resources/views/test/ant.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>gallery of Amornthep 65122420004</h1>
+        <h1><img src={{$ant}}></h1>
+    </body>
+    </html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h1>gallery of Amornthep 65122420004</h1>
+        <p1>ant</p1><a href="gallery/ant"><img style="width: 250px;" height="250px;" src="{{ $ant }}"alt="#"></a>
+        <p1>bird<a href="gallery/bird"><img style="width: 250px;" height="250px;" src="{{ $bird }}"alt="#">
+        <p1>cat<a href="gallery/cat"><img style="width: 250px;" height="250px;" src="{{ $cat }}"alt="#">
+        <p1>god<a href="gallery/god"><img style="width: 250px;" height="250px;" src="{{ $god }}"alt="#">
+        <p1>spider<a href="gallery/spider"><img style="width: 250px;" height="250px;" src="{{ $spider }}"alt="#">
+    </body>
+    </html>
